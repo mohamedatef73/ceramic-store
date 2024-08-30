cb2ed4a390cbafc1c7ef49ebc1675680aca937be
mohamedatef73 <matef4202@gmail.com>
2024-08-30 19:36:37 +0300

    fix errors

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+// next.config.js
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  swcMinify: true,
+});
+
+  
