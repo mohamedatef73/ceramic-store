6e3ea07dd8fc0176ff23d2be7088c8d15993a3c7
mohamedatef73 <matef4202@gmail.com>
2024-09-01 01:39:04 +0300

    update

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -3,16 +3,13 @@
 
 // module.exports = nextConfig
 // next.config.js
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-// module.exports = withBundleAnalyzer({
-//   reactStrictMode: true,
-//   swcMinify: true,
-// });
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  swcMinify: true,
+});
 
- /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+  
