a53ff58ce3b6f666b659de56b17b58de1d431370
mohamedatef73 <matef4202@gmail.com>
2024-09-01 00:29:48 +0300

    update code

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -3,13 +3,22 @@
 
 // module.exports = nextConfig
 // next.config.js
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-  swcMinify: true,
-});
+// module.exports = withBundleAnalyzer({
+//   reactStrictMode: true,
+//   swcMinify: true,
+// });
+
+// next.config.js
+module.exports = {
+  webpack: (config) => {
+    config.cache = false;
+    return config;
+  },
+};
+
 
   
--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -1,10 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {important: true
-,
-  content: [],
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {important: true
+// ,
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1DA1F2',
+      },
+    },
   },
   plugins: [],
-}
-
+};
