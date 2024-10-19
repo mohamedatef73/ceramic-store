c05ceff1cbf902ebf4446eafe5a0db5d1bc19f2e
mohamedatef73 <matef4202@gmail.com>
2024-10-19 14:55:18 +0300

    delete code

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,8 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
-// next.config.js
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
