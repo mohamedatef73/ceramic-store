e2222fedf184ef716dc6d108bbc04d9ff52558f5
mohamedatef73 <matef4202@gmail.com>
2024-10-21 19:46:54 +0300

    fix

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  // Basic example with no custom configuration
-  reactStrictMode: true,
-};
+// module.exports = {
+//   // Basic example with no custom configuration
+//   reactStrictMode: true,
+// };
 // next.config.js
 
+module.exports = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL, // If needed to expose this env var to the client (though typically, you don't need to).
+  },
+  reactStrictMode: true,
+}
