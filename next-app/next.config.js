68520a4381f3fa2c940a76dedf0280c767fe2cd7
mohamedatef73 <matef4202@gmail.com>
2024-10-21 19:39:07 +0300

    fix errors

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -4,9 +4,3 @@ module.exports = {
 };
 // next.config.js
 
-module.exports = {
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL, // If needed to expose this env var to the client (though typically, you don't need to).
-  },
-  reactStrictMode: true,
-}
