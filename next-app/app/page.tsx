db0368ea2c640889f047447d16dbd846e7519cda
mohamed atef <59102121+mohamedatef73@users.noreply.github.com>
2024-10-27 11:34:57 +0300

    Update page.tsx

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -2,6 +2,7 @@ import Camp from "../components/Camp";
 import Guid from "../components/Guid";
 import Heros from "../components/Heros";
 import Carousel from "../components/Carousel";
+import CardProducts from "../components/CardProducts"
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <Heros />
       <Camp />
       <Guid />
+      <CardProducts/>
+
     </>
   );
 }
