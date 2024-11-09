7a8a6a0e4e4f77a99d6601b6a43e69653e81a495
mohamedatef73 <matef4202@gmail.com>
2024-11-09 20:51:32 +0200

    delete file

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,7 +1,4 @@
 
 module.exports = {
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL, // If needed to expose this env var to the client (though typically, you don't need to).
-  },
   reactStrictMode: true,
 }
