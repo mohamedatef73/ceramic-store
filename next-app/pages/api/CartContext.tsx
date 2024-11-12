f3e10f05abab22988e7cfd1dca2d3c2e15a15366
mohamedatef73 <matef4202@gmail.com>
2024-11-12 20:46:16 +0200

    update

--- next-app/pages/api/CartContext.tsx
+++ next-app/pages/api/CartContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useState, ReactNode } from 'react';
 import { Product } from '../../constants';
 
 interface CartContextType {
